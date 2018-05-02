1c459842bde205dd8acb39cb5749ea5b63207cdf
ZooeyBossert <zooey.bossert@gmail.com>
2018-05-02 12:53:12 +0200

    tot aan part 7

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,13 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Zooey Bossert on 02-05-18.
+//  Copyright © 2018 Zooey Bossert. All rights reserved.
+//
+
+import UIKit
+
+class ToDoCell: UITableViewController {
+    
+}
